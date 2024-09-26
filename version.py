e0cea7182410ac273a5f85a9b18872a7638ee6a8
qiqidedashamiao <zl_11191224@163.com>
2024-09-27 07:31:49 +0800

    设置脚本断点

--- version.py
+++ version.py
@@ -1,2 +1,9 @@
 import sys
 print(sys.version)
+import gdb
+
+gdb.execute("break *digui")
+gdb.execute("run")
+
+the_line = gdb.execute("info registers rip", to_string=True)
+gdb.execute("dump memory memory.dump $rip $rip+100")
