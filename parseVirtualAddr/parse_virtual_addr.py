a59e3b8a4393b4523935a85ace236c5fd54a15fb
zhaqi <zha-qi@foxmail.com>
2025-07-10 00:11:23 +0800

    获取libc的mapping地址

--- parseVirtualAddr/parse_virtual_addr.py
+++ parseVirtualAddr/parse_virtual_addr.py
@@ -7,6 +7,7 @@ import sys
 import readline
 import os
 import atexit
+import time
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 #添加上一层目录到模块搜索路径
 current_dir = os.path.dirname(os.path.abspath(__file__)) 
@@ -31,9 +32,15 @@ class CoreFile:
         # 判断目录data是否存在，如果不存在则创建
         if not os.path.exists('data'):
             os.mkdir('data')
-        self.loadmap = {}
+        self.loadmap = {} # key:虚拟地址起始地址,16进制, value:LOAD信息分割
         self.loadzeromap = {}
         self.gdb_process = None
+        self.heap_info = None
+        self.main_top = None
+        self.main_arean = None
+        self.sub_arean = []
+        self.map_libc = {} # key:libc.so的起始地址,16进制, value:maps信息分割
+
         pass
 
     # 定义一个析构函数
@@ -124,9 +131,22 @@ class CoreFile:
         # 如果addr不在loadmap中，则返回None
         return None,None
 
-    # 定义一个方法，查找core文件包含当前虚拟地址的虚拟地址
-    def find_virtual_address(self, addr):
+    # 检测虚拟地址addr是否在栈地址上
+    def check_stack_address(self, addr):
+        # 遍历loadmap，如果addr在loadmap中，则返回loadmap中的值
+        for key,lines in self.loadmap.items():
+            addrint = int(addr, 16)
+            if addrint >= int(lines[2], 16) and addrint <= int(lines[8], 16):
+                size = int(lines[5], 16)
+                if size == self.config.get_stack_size():
+                    return True,key,lines
+        # 如果addr不在loadmap中，则返回None
+        return False,None,None
+
+    # 初始化core文件，获取loadmap信息
+    def init_core(self):
         if self.loadmap == {}:
+            print("init core file")
             self.get_load_info()
         if self.gdb_process is None:
             self.gdb_process = start_gdb_core(f"{self.config.get_cross()}gdb", self.config.get_sonia_path(), self.config.get_core_path(),False)
@@ -138,6 +158,65 @@ class CoreFile:
             if self.config.get_lib_path() != '':
                 res = run_command(self.gdb_process, f'set solib-search-path {self.config.get_lib_path()}')
                 # print(res)
+
+     #读文件进行偏移操作
+    def read_file(self):
+        initial_offset = int(self.heap_info[1], 16) + 8
+        
+        total_size = int(self.heap_info[5], 16)
+        print(f"initial_offset:{hex(initial_offset)}\ntotal_size: {hex(total_size)}")
+        offset = 8
+        cur_offset = 0
+        n = 0
+        start_time = time.time()
+        temp_time = start_time
+        try:
+            with open(self.config.get_core_path(), 'rb') as file:
+                # 1. 定位到初始偏移位置
+                file.seek(initial_offset)
+                count = 0
+                while offset < total_size:
+                    # 2. 读取8字节（64位）小端序数据
+                    data = file.read(8)
+                    if len(data) != 8:
+                        raise ValueError(f"无法读取16字节数据，文件可能已结束 (位置: {offset+initial_offset})")
+                    
+                    # 3. 将小端序字节转换为整数
+                    size = int.from_bytes(data, byteorder='little', signed=False) & 0xFFFFFFFFFFFFFFF8
+                    # print(f"读取的8字节值: {hex(size)} -> 整数 szie = {size}")
+                    # vitual_addr = hex(int(self.heap_info[2], 16)+offset)
+                    # print(f"vitual_addr: {vitual_addr}")
+                    
+                    if size < 32:
+                        print(f"内存异常")
+                        break
+                    # 4. 计算最终偏移位置
+                    # current_pos = file.tell()  # 当前位置（已读取8字节后）
+                    # final_offset = current_pos + n
+                    count += 1
+                    if count%1000 == 0:
+                        mid_time = (time.time() - temp_time)
+                        temp_time = time.time()
+                        physical_addr = hex(offset + initial_offset)
+                        vitual_addr = hex(int(self.heap_info[2], 16)+offset)
+                        print(f"count:{count}, mid_time:{mid_time}, physical_addr:{physical_addr},  vitual_addr:{vitual_addr}, size:{hex(size)}")
+                    # if count==5:
+                    #     break
+                    offset += size
+                    file.seek(size-8, 1)  # 从当前位置偏移n字节
+                #0x2b2bed20  282734
+                top_vitual_addr = hex(int(self.heap_info[2], 16)+offset-size-8)
+                self.main_top = top_vitual_addr
+                print(f"top_vitual_addr: {top_vitual_addr} size: {hex(size)} count: {count}")
+                end_time = time.time()
+                print(f"处理文件耗时: {end_time - start_time} 秒")
+        except Exception as e:
+            print(f"操作失败: {str(e)}")
+            raise
+
+    # 定义一个方法，查找core文件包含当前虚拟地址的虚拟地址
+    def find_virtual_address(self, addr):
+        self.init_core()
         
         #遍历每一个loadmap，通过gdb的find命令查找包含addr的虚拟地址，lines[2]为虚拟地址起始地址，lines[8]为虚拟地址结束地址
         for key,lines in self.loadmap.items():
@@ -167,48 +246,79 @@ class CoreFile:
                     value_key,values_lines = self.check_virtual_address(value)
                     print(f"values_lines:{values_lines}") 
 
-    # 定义一个方法，查找core文件包含当前虚拟地址的虚拟地址
-    def find_virtual_address_end(self, addr):
-        if self.loadmap == {}:
-            self.get_load_info()
-        if self.gdb_process is None:
-            self.gdb_process = start_gdb_core(f"{self.config.get_cross()}gdb", self.config.get_sonia_path(), self.config.get_core_path(),False)
-            if self.gdb_process is None:
-                print("gdb start failed")
-                return False
-            print("gdb start success")
-            #加载lib
-            if self.config.get_lib_path() != '':
-                res = run_command(self.gdb_process, f'set solib-search-path {self.config.get_lib_path()}')
-                # print(res)
+
+    '''
+    LOAD           0x0000000000bed000 0x0000000029127000 0x0000000000000000
+                 0x00000000021b8000 0x00000000021b8000  RW     0x1000
+        libc.so.6
+        0x7f88fdd000       0x7f89161000   0x184000        0x0 /lib64/libc.so.6
+        0x7f89161000       0x7f89171000    0x10000   0x184000 /lib64/libc.so.6
+        0x7f89171000       0x7f89175000     0x4000   0x184000 /lib64/libc.so.6
+        0x7f89175000       0x7f89177000     0x2000   0x188000 /lib64/libc.so.6
+
+        addr:  0x7f891759b8
+values_lines:['LOAD', '0x0000000055240000', '0x0000007f89175000', '0x0000000000000000', '0x0000000000002000', '0x0000000000002000', 'RW', '0x1000', '0x7f89177000']
+find_virtual_address end: 0x2b2bed20
+    '''
+    '''
+    next:0x0000007f28000030    top:0x0000007f28000030+96
+    next:0x0000007f28000030+2176+24-40
+    '''
+    # 获取堆的top指针
+    def find_top(self):
+        self.init_core()
+        print(f"heap_info:{self.heap_info}")
+        self.heap_info = self.heap_info.split()
+        startaddr = int(self.heap_info[2], 16)
+        endaddr = startaddr + int(self.heap_info[5], 16) + 8
+        nextaddr = startaddr + 8
+        print(f"endaddr: {hex(endaddr)}")
+        self.read_file()
         
-        #遍历每一个loadmap，通过gdb的find命令查找包含addr的虚拟地址，lines[2]为虚拟地址起始地址，lines[8]为虚拟地址结束地址
-        for key,lines in self.loadmap.items():
-            # print(f"key: {key}")
-            cmdline = f'find {lines[2]},{lines[8]},{addr}'
-            res = run_command(self.gdb_process, cmdline, False)
-            # print(res)
-            # print(f"res: {res}")
-            if "Pattern not found" in res:
-                continue
-            else:
-                print(cmdline)
-                print(f"{res}")
-                # print(lines)
-                '''
-                0x7f70076798
-                0x7f700767a0
-                0x7f7008ae20
-                3 patterns found.
-                '''
-                # 解析输出结果，判断每一个输出结果所在的虚拟地址范围
-                values = res.split('\n')
-                values.pop()
-                values.pop()
-                for value in values:
-                    print(f"addr: {value}")
-                    value_key,values_lines = self.check_virtual_address(value)
-                    print(f"values_lines:{values_lines}") 
+    '''
+main_area:0x7f89175958
+next: 0x0000007f28000030
+next: 0x0000007f30000030
+next: 0x0000007f34000030
+next: 0x0000007f3c000030
+next: 0x0000007f40000030
+next: 0x0000007f48000030
+next: 0x0000007f4c000030
+next: 0x0000007f54000030 
+next: 0x0000007f60000030
+next: 0x0000007f64000030
+next: 0x0000007f6c000030
+next: 0x0000007f70000030
+next: 0x0000007f7c000030
+next: 0x0000007f78000030
+next: 0x0000007f80000030
+main_area: 0x0000007f89175958
+根据主分区的top指针，找到libc.so库的main_area的top的地址，然后计算出main_area的地址，然后计算出各个子分配区的arean指针，以及计算出各个子分配区的top指针，各个子分配区的大小
+'''
+    # def find_arean(self, main_top_addr):
+    #     self.main_arean = int(main_top_addr, 16)-96
+    #     print(f"main_arean: {hex(self.main_arean)}")
+    #     # next = self.main_arean + 2160
+
+
+    def get_mapping(self):
+        cmdline = "info proc mappings"
+        res = run_command(self.gdb_process, cmdline, False)
+        lines = res.split('\n')
+        lines.pop()
+        for line in lines:
+            if 'libc.so' in line:
+                # print(line)
+                # 解析出libc.so的地址
+                parts = line.split()
+                self.map_libc[int(parts[0],16)] = license
+                if len(parts) >= 3:
+                    self.heap_info = parts[0:6]
+                    print(f"heap_info: {self.heap_info}")
+                    break
+        
+
+    
 
 class Config:
     """配置类，用于存储配置信息"""
@@ -343,7 +453,7 @@ if __name__ == "__main__":
     # 创建带Tab补全的提示器
     # 这里可以添加自定义的补全选项
     sp = SimplePrompt(completions=[
-        'help', 'exit', 'quit', 'config', 'type', 'setcross', 'setsonia', 'setcore', 'setparam', 'setlib',  'find', 'findend',
+        'help', 'exit', 'quit', 'config', 'type', 'setcross', 'setsonia', 'setcore', 'setparam', 'setlib',  'find', 'findtop',
     ])
 
     # 命令解释
@@ -433,20 +543,11 @@ if __name__ == "__main__":
                 if not config.valid():
                     print("配置错误，请检查配置：交叉编译链、sonia路径、core文件路径")
                     print( f"cross: {config.get_cross()}\nsonia_path: {config.get_sonia_path()}\ncore_path: {config.get_core_path()}\n" )
-                else:
                     if coreFile is None:
                        coreFile = CoreFile(config)
                     coreFile.find_virtual_address(params[1])
 
-        elif cmd.lower() == 'findend':
-            if len(params) != 2:
-                print("参数错误")
-                print("findend startaddr,endaddr,addr - 查找包含当前符号的虚拟地址，直到找到全局变量或者堆栈地址")
-            else:
-                if not config.valid():
-                    print("配置错误，请检查配置：交叉编译链、sonia路径、core文件路径")
-                    print( f"cross: {config.get_cross()}\nsonia_path: {config.get_sonia_path()}\ncore_path: {config.get_core_path()}\n" )
-                else:
-                    if coreFile is None:
-                        coreFile = CoreFile(config)
-                    coreFile.find_virtual_address_end(params[1])
+        elif cmd.lower() == 'findtop':
+            if coreFile is None:
+                coreFile = CoreFile(config)
+            coreFile.find_top()
